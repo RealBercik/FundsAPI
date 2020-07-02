5e8fb7c3331a8fc8022b826e13210aecd8ec76d2
Albert Bielecki <albert.bielecki@cam-systems.co.uk>
2020-07-02 22:48:14 +0100

    Added FundsLogic class.

---
+++ Api/Logic/FundsLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.DataFiles;
+
+namespace Api.Logic
+{
+    public class FundsLogic
+    {
+        public async Task<IList<FundDetails>> GetFunds()
+        {
+            return new List<FundDetails>();
+        }
+    }
+}
